cb6b48a42ed42c2350c35b6c67cc74eefa423a61
Kevin Ding <aimixiu1993@gmail.com>
2021-03-14 14:48:08 -0400

    Server table

--- src/components/servers/index.js
+++ src/components/servers/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DataTable from '../table';
+
+const columns = [
+  { key: 'ip4', label: 'Ip4' },
+  { key: 'dns', label: 'Dns' },
+];
 
 const Servers = ({ servers }) => {
-  console.log(servers);
-  return <div>Servers</div>;
+  return <DataTable columns={columns} rows={servers} tableName={'servers'} />;
 };
 
 Servers.propTypes = {
--- src/components/surfaces/index.js
+++ src/components/surfaces/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DataTable from '../table';
 
-const Surfaces = ({ surfaces }) => {
-  const columns = [
-    { key: 'venueName', label: 'Venue Name' },
-    { key: 'surfaceName', label: 'Surface Name' },
-    { key: 'sport', label: 'Sport' },
-    { key: 'status', label: 'Status' },
-  ];
+const columns = [
+  { key: 'venueName', label: 'Venue Name' },
+  { key: 'surfaceName', label: 'Surface Name' },
+  { key: 'sport', label: 'Sport' },
+  { key: 'status', label: 'Status' },
+];
 
+const Surfaces = ({ surfaces }) => {
   return <DataTable columns={columns} rows={surfaces} tableName={'surfaces'} />;
 };
 
